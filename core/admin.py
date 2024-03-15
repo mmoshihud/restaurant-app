6d87ee98692f9a8ff671c57ec465d181085dfa4f
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2024-03-16 03:42:08 +0600

    Add custom user admin forms and register new UserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,124 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-from .models import User
+from .models import User as MyUser
 
-admin.site.register(User)
+
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+
+    username = forms.CharField(
+        label="Username",
+        max_length=100,
+        widget=forms.TextInput(attrs={"class": "special"}),
+    )
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = MyUser
+        fields = ["email", "username"]
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """A form for updating users. Includes all the fields on
+    the user, but replaces the password field with admin's
+    disabled password hash display field.
+    """
+
+    password = ReadOnlyPasswordHashField()
+    new_password = forms.CharField(
+        label="New Password", widget=forms.PasswordInput, required=False
+    )
+
+    class Meta:
+        model = MyUser
+        fields = ["email", "password", "new_password", "is_active", "is_admin"]
+
+    def clean_password(self):
+        """
+        Regardless of what the user provides, return the initial value.
+        This is done here, rather than on the field, because the
+        field does not have access to the initial value.
+        """
+        return self.initial["password"]
+
+    def clean_new_password(self):
+        new_password = self.cleaned_data.get("new_password")
+        if new_password:
+            # You can add your password validation logic here if needed
+            pass
+        return new_password
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        new_password = self.cleaned_data.get("new_password")
+        if new_password:
+            user.set_password(new_password)
+            if commit:
+                user.save()
+        return user
+
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ["email", "username", "is_admin", "is_active"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["email", "password", "new_password"]}),
+        ("Personal info", {"fields": ["username", "first_name", "last_name"]}),
+        (
+            "Permissions",
+            {"fields": ["is_superuser", "is_admin", "is_active"]},
+        ),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "username", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+# Now register the new UserAdmin...
+admin.site.register(MyUser, UserAdmin)
+# ... and, since we're not using Django's built-in permissions,
+# unregister the Group model from admin.
+admin.site.unregister(Group)
